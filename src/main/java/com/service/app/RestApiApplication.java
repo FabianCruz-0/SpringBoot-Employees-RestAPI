16831c30c086cc5f38899cdbc656ba9cfd61d92a
Fabián Cruz <fabiancruz3773@gmail.com>
2021-08-25 14:50:52 -0500

    arrego de objetos

--- src/main/java/com/service/app/RestApiApplication.java
+++ src/main/java/com/service/app/RestApiApplication.java
@@ -18,24 +18,22 @@ public class RestApiApplication {
 	@GetMapping
 	public List<Empleado> hello()
 	{
-		return List.of(
-				new Empleado(1, "Fabián", 20, 7000),
-				new Empleado(2, "Marco", 34, 6500),
-				new Empleado(3, "Jesus", 18, 4500),
-				new Empleado(4, "Alberto", 60, 25000),
-				new Empleado(5, "María", 28, 50000),
-				new Empleado(6, "Sabrina", 22, 650000),
-				new Empleado(7, "Debanhi", 38, 10000),
-				new Empleado(8, "Mariana", 45, 5000),
-				new Empleado(9, "Eliud", 63, 8000),
-				new Empleado(10, "Jose", 76, 5000),
-				new Empleado(11, "Jaime", 17, 7400),
-				new Empleado(12, "Kevin", 68, 450000),
-				new Empleado(13, "Alvarez", 90, 23000),
-				new Empleado(14, "Cesar", 23, 12000),
-				new Empleado(15, "Mauricio", 66, 52000)
-				);
-		
+		Empleado em1 = new Empleado(1, "Fabián", 20, 7000);
+		Empleado em2 =new Empleado(2, "Marco", 34, 6500);
+		Empleado em3= new Empleado(3, "Jesus", 18, 4500);
+		Empleado em4= new Empleado(4, "Alberto", 60, 25000);
+		Empleado em5=new Empleado(5, "María", 28, 50000);
+		Empleado em6=new Empleado(6, "Sabrina", 22, 650000);
+		Empleado em7=new Empleado(7, "Debanhi", 38, 10000);
+		Empleado em8=new Empleado(8, "Mariana", 45, 5000);
+		Empleado em9=new Empleado(9, "Eliud", 63, 8000);
+		Empleado em10=new Empleado(10, "Jose", 76, 5000);
+		Empleado em11=new Empleado(11, "Jaime", 17, 7400);
+		Empleado em12=new Empleado(12, "Kevin", 68, 450000);
+		Empleado em13=new Empleado(13, "Alvarez", 90, 23000);
+		Empleado em14=new Empleado(14, "Cesar", 23, 12000);
+		Empleado em15=new Empleado(15, "Mauricio", 66, 52000);
+		return List.of(em1,em2,em3,em4,em5,em6,em7,em8,em9,em10,em11,em12,em12,em13,em14,em15);
 	}
 }
 
